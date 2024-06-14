6c808b516a7022eec3c0befe07e3fbf07235fec9
Viktor Rura <ruravictor@gmail.com>
2024-06-14 21:33:16 +0100

    Configured settings file to manage all dependencies and created property app as well as property model.

---
+++ property/models.py
@@ -0,0 +1,24 @@
+import uuid
+
+from django.conf import settings
+from django.db import models
+
+from useraccount.models import User
+
+
+class Property(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.IntegerField()
+    bedrooms = models.PositiveIntegerField()
+    bathrooms = models.PositiveIntegerField()
+    guests = models.PositiveIntegerField()
+    country = models.CharField(max_length=255)
+    country_code = models.CharField(max_length=10)
+    category = models.CharField(max_length=255)
+    #favorite
+    image = models.ImageField(upload_to="uploads/properties")
+    landlord = models.ForeignKey(User, related_name="properties", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+

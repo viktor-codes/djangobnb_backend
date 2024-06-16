8b81a382ae4c7e9194e23cd656e6040259b5f41f
Viktor Rura <ruravictor@gmail.com>
2024-06-16 16:27:53 +0100

    Added form to create new property

--- property/api.py
+++ property/api.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 
+from .forms import PropertyForm
 from .models import Property
 from .serializers import PropertiesListSerializer
 
@@ -16,3 +17,18 @@ def properties_list(request):
     return JsonResponse({
         "data": serializer.data,
     })
+
+
+@api_view(["POST", "FILES"])
+def create_property(request):
+    form = PropertyForm(request.POST, request.FILES)
+
+    if form.is_valid():
+        property = form.save(commit=False)
+        property.landlord = request.user
+        property.save()
+
+        return JsonResponse({"success": True})
+    else:
+        print("error", form.errors, form.non_field_errors())
+        return JsonResponse({"errors": form.errors.as_json()}, status=400)
--- property/urls.py
+++ property/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import api
 
 urlpatterns = [
-    path("", api.properties_list, name="properties_list")
+    path("", api.properties_list, name="properties_list"),
+    path("create/", api.create_property, name="create_property"),
 ]

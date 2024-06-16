401f6bb88b25ead0fcaa9d243d69aec40e584036
Viktor Rura <ruravictor@gmail.com>
2024-06-16 21:02:52 +0100

    Added Reservations to admin panel,created endpoint to make a booking.

--- property/api.py
+++ property/api.py
@@ -7,7 +7,7 @@ from rest_framework.decorators import (
 )
 
 from .forms import PropertyForm
-from .models import Property
+from .models import Property, Reservation
 from .serializers import PropertiesListSerializer, PropertiesDetailSerializer
 
 
@@ -46,3 +46,30 @@ def create_property(request):
     else:
         print("error", form.errors, form.non_field_errors())
         return JsonResponse({"errors": form.errors.as_json()}, status=400)
+
+
+@api_view(["POST"])
+def book_property(request, pk):
+    try:
+        start_date = request.POST.get("start_date", "")
+        end_date = request.POST.get("end_date", "")
+        number_of_nights = request.POST.get("number_of_nights", "")
+        total_price = request.POST.get("total_price", "")
+        guests = request.POST.get("guests", "")
+
+        property = Property.objects.get(pk=pk)
+
+        Reservation.objects.create(
+            property=property,
+            start_date=start_date,
+            end_date=end_date,
+            number_of_nights=number_of_nights,
+            total_price=total_price,
+            guests=guests,
+            created_by=request.user
+        )
+    except Exception as e:
+        print("Error", e)
+
+        return JsonResponse({"success": False}, status=400)
+    
--- property/urls.py
+++ property/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import api
 
 urlpatterns = [
-    path("", api.properties_list, name="properties_list"),
-    path("create/", api.create_property, name="create_property"),
-    path("<uuid:pk>/", api.properties_detail, name="properties_detail"),
+    path("", api.properties_list, name="api_properties_list"),
+    path("create/", api.create_property, name="api_create_property"),
+    path("<uuid:pk>/", api.properties_detail, name="api_properties_detail"),
+    path("<uuid:pk>/book/", api.book_property, name="api_book_property"),
 ]

a20fe5c9618a08c176bbaf2612348a39066254a1
Viktor Rura <ruravictor@gmail.com>
2024-06-18 14:46:47 +0100

    Added conversation detail serializer and view to handle messages.

--- chat/api.py
+++ chat/api.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 
 from .models import Conversation
-from .serializers import ConversationListSerializer
+from .serializers import ConversationListSerializer, ConversationDetailSerializer
 
 
 @api_view(['GET'])
@@ -12,3 +12,14 @@ def conversations_list(request):
         request.user.conversations.all(), many=True)
 
     return JsonResponse(serializer.data, safe=False)
+
+
+@api_view(['GET'])
+def conversations_detail(request, pk):
+    conversation = request.user.conversations.get(pk=pk)
+    conversation_serializer = ConversationDetailSerializer(conversation, many=False)
+
+    return JsonResponse({
+        'conversation': conversation_serializer.data,
+        
+    }, safe=False)
--- chat/serializers.py
+++ chat/serializers.py
@@ -9,3 +9,11 @@ class ConversationListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Conversation
         fields = ('id', 'users', 'created_at', 'modified_at')
+
+
+class ConversationDetailSerializer(serializers.ModelSerializer):
+    users = UserDetailSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Conversation
+        fields = ('id', 'users', 'created_at', 'modified_at')

f6db5e94db086212d5c60354ac38e845ec63cfad
Viktor Rura <ruravictor@gmail.com>
2024-06-18 12:08:30 +0100

    Added models and registered admin for chat app.

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Convesation, ConversationMessage
+
+admin.site.register(Convesation)
+admin.site.register(ConversationMessage)
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,23 @@
+import uuid
 from django.db import models
+from useraccount.models import User
 
-# Create your models here.
+
+class Convesation(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    users = models.ManyToManyField(User, related_name='conversations')
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
+class ConversationMessage(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    conversation = models.ForeignKey(
+        Convesation, on_delete=models.CASCADE, related_name='messages')
+    body = models.TextField()
+    sent_to = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='received_messages')
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='sent_messages')
+    created_at = models.DateTimeField(auto_now_add=True)
+   
